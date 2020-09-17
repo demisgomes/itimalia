4626cc286575b5bafbc7eb89343c4b367691ee4b
Demis Gomes <demismg72@gmail.com>
2020-09-17 18:30:26 -0300

    replacing getProperty by getenv

--- src/main/kotlin/com/abrigo/itimalia/application/config/EnvironmentConfig.kt
+++ src/main/kotlin/com/abrigo/itimalia/application/config/EnvironmentConfig.kt
@@ -3,19 +3,19 @@ package com.abrigo.itimalia.application.config
 object EnvironmentConfig {
 
     fun jdbcUrl(): String {
-        return getProperty("JDBC_URL", "jdbc:h2:mem:test;MODE=MySQL")
+        return getenv("JDBC_URL", "jdbc:h2:mem:test;MODE=MySQL")
     }
 
-    private fun getProperty(property: String, defaultValue: String): String {
-        return if (System.getProperty(property) != null) System.getProperty(property)
+    private fun getenv(env: String, defaultValue: String): String {
+        return if (System.getenv(env) != null) System.getenv(env)
         else defaultValue
     }
 
     fun databaseUsername() : String{
-        return getProperty("DATABASE_USERNAME", "sa")
+        return getenv("DATABASE_USERNAME", "sa")
     }
 
     fun databasePassword() : String{
-        return getProperty("DATABASE_PASSWORD", "")
+        return getenv("DATABASE_PASSWORD", "")
     }
 }
