0a69abf74da6d055112c972d121a54e188850312
Demis Gomes <demismg72@gmail.com>
2021-02-27 02:54:01 -0300

    Updating Swagger docs to implement auth feature

--- src/main/kotlin/com/abrigo/itimalia/application/web/swagger/SwaggerAnimalDocumentation.kt
+++ src/main/kotlin/com/abrigo/itimalia/application/web/swagger/SwaggerAnimalDocumentation.kt
@@ -2,21 +2,37 @@ package com.abrigo.itimalia.application.web.swagger
 
 import com.abrigo.itimalia.domain.entities.animal.Animal
 import com.abrigo.itimalia.domain.entities.animal.NewAnimal
+import com.abrigo.itimalia.domain.exceptions.ErrorResponse
 import io.javalin.plugin.openapi.dsl.OpenApiDocumentation
+import io.swagger.v3.oas.models.security.SecurityRequirement
 
 object SwaggerAnimalDocumentation{
     fun createAnimalDocumentation() = OpenApiDocumentation()
         .body<NewAnimal>(CONTENT_TYPE)
         .json("200", Animal::class.java)
+        .result("403", ErrorResponse::class.java)
+        .operation {
+            it.addSecurityItem(SecurityRequirement().addList("BearerAuth"))
+        }
 
     fun updateAnimalDocumentation() = OpenApiDocumentation()
         .pathParam("id", String::class.java)
         .body<Animal>(CONTENT_TYPE)
         .json("200", Animal::class.java)
+        .result("403", ErrorResponse::class.java)
+        .result("404", Unit::class.java)
+        .operation {
+            it.addSecurityItem(SecurityRequirement().addList("BearerAuth"))
+        }
 
     fun deleteAnimalDocumentation() = OpenApiDocumentation()
         .pathParam("id", String::class.java)
         .result("204", null)
+        .result("403", ErrorResponse::class.java)
+        .result("404", Unit::class.java)
+        .operation {
+            it.addSecurityItem(SecurityRequirement().addList("BearerAuth"))
+        }
 
     fun getAnimalDocumentation()= OpenApiDocumentation()
         .pathParam("id", String::class.java)
@@ -31,4 +47,10 @@ object SwaggerAnimalDocumentation{
     fun adoptAnimalDocumentation() = OpenApiDocumentation()
         .pathParam("id", String::class.java)
         .json("200", Animal::class.java)
+        .result("403", ErrorResponse::class.java)
+        .result("404", Unit::class.java)
+        .result("410", Unit::class.java)
+        .operation {
+            it.addSecurityItem(SecurityRequirement().addList("BearerAuth"))
+        }
 }
--- src/main/kotlin/com/abrigo/itimalia/application/web/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/abrigo/itimalia/application/web/swagger/SwaggerConfig.kt
@@ -6,24 +6,46 @@ import io.javalin.core.security.SecurityUtil.roles
 import io.javalin.plugin.openapi.OpenApiOptions
 import io.javalin.plugin.openapi.OpenApiPlugin
 import io.javalin.plugin.openapi.ui.SwaggerOptions
+import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityScheme
 
 const val CONTENT_TYPE = "application/json"
 
-object SwaggerConfig{
+object SwaggerConfig {
+
+    private fun getSecurityScheme(): SecurityScheme {
+        val securityScheme = SecurityScheme()
+        securityScheme.type = SecurityScheme.Type.HTTP
+        securityScheme.scheme = "bearer"
+        securityScheme.bearerFormat = "JWT"
+
+        return securityScheme
+    }
 
     fun registerPlugin(config: JavalinConfig): JavalinConfig {
         return config.registerPlugin(OpenApiPlugin(getOpenApiOptions()))
     }
 
     private fun getOpenApiOptions(): OpenApiOptions {
-        val applicationInfo = Info()
-            .version("1.0")
-            .description("Itimalia Application")
+        val initialConfigurationCreator = {
+            OpenAPI()
+                .info(Info().version("1.0").description("Itimalia Application").title("API Documentation"))
+                .schemaRequirement("BearerAuth", getSecurityScheme())
+        }
 
-        return OpenApiOptions(applicationInfo)
-            .path("/swagger-docs")
-            .swagger(SwaggerOptions("/swagger"))
-            .roles(roles(RouteRole.ANYONE))
+        val openApiOptions = OpenApiOptions(initialConfigurationCreator)
+        openApiOptions.path("/swagger-docs")
+        openApiOptions.swagger(SwaggerOptions("/swagger").title("Itimalia Documentation")).roles(roles(RouteRole.ANYONE))
+
+        return openApiOptions
     }
+//        val applicationInfo = Info()
+//            .version("1.0")
+//            .description("Itimalia Application")
+//
+//        return OpenApiOptions(applicationInfo)
+//            .path("/swagger-docs")
+//            .swagger(SwaggerOptions("/swagger"))
+//            .roles(roles(Roles.ANYONE))
 }
