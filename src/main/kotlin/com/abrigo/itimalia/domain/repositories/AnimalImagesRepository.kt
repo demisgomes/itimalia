7ca5ce27520f24ac279c7a307542f419774be9df
Demis Gomes <demis.gomes@thoughtworks.com>
2022-01-25 09:37:41 -0300

    Add tests to AnimalImagesRepository.kt

--- src/main/kotlin/com/abrigo/itimalia/domain/repositories/AnimalImagesRepository.kt
+++ src/main/kotlin/com/abrigo/itimalia/domain/repositories/AnimalImagesRepository.kt
@@ -3,6 +3,5 @@ package com.abrigo.itimalia.domain.repositories
 import com.abrigo.itimalia.domain.entities.image.Image
 
 interface AnimalImagesRepository {
-    fun add(newImage: Image, newAnimalId: Int)
     fun addAll(newImages: List<Image>, newAnimalId: Int)
 }
--- src/main/kotlin/com/abrigo/itimalia/resources/storage/exposed/gateways/AnimalImagesRepositoryImpl.kt
+++ src/main/kotlin/com/abrigo/itimalia/resources/storage/exposed/gateways/AnimalImagesRepositoryImpl.kt
@@ -1,36 +1,33 @@
 package com.abrigo.itimalia.resources.storage.exposed.gateways
 
 import com.abrigo.itimalia.domain.entities.image.Image
+import com.abrigo.itimalia.domain.exceptions.AnimalNotFoundException
 import com.abrigo.itimalia.domain.repositories.AnimalImagesRepository
 import com.abrigo.itimalia.resources.storage.exposed.entities.AnimalImageEntity
 import com.abrigo.itimalia.resources.storage.exposed.entities.AnimalMap
 import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class AnimalImagesRepositoryImpl : AnimalImagesRepository{
-    override fun add(newImage: Image, newAnimalId: Int) {
-        transaction {
-           AnimalImageEntity.new {
-               name = newImage.name
-               path = newImage.path
-               format = newImage.format
-               bytes = newImage.bytes
-               animalId = EntityID(newAnimalId, AnimalMap)
-           }
-       }
-    }
-
     override fun addAll(newImages: List<Image>, newAnimalId: Int) {
-        transaction {
-            newImages.forEach {
-                AnimalImageEntity.new {
-                    name = it.name
-                    path = it.path
-                    format = it.format
-                    bytes = it.bytes
-                    animalId = EntityID(newAnimalId, AnimalMap)
+
+        try{
+            transaction {
+                newImages.forEach {
+                    AnimalImageEntity.new {
+                        name = it.name
+                        path = it.path
+                        format = it.format
+                        bytes = it.bytes
+                        animalId = EntityID(newAnimalId, AnimalMap)
+                    }
                 }
             }
         }
+        catch (exception: ExposedSQLException) {
+            throw AnimalNotFoundException()
+        }
+
     }
 }
